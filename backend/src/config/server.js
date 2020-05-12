b64c346af85ea007ac9a475bfb2634ef48986c99
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2020-05-12 10:21:04 -0300

    Configuração servidor express
    Configuração servidor express

---
+++ backend/src/config/server.js
@@ -0,0 +1,12 @@
+const port = 3003
+
+const bodyparser = require('body-parser')
+const express = require('express')
+const server = express()
+
+server.use(bodyparser.urlencoded({extend: true}))
+server.use(bodyparser.json())
+
+server.listen(port, function(){
+  console.log('rodando na porta 3003')
+})
