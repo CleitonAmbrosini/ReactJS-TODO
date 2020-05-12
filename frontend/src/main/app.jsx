7b9ca290ca40087d1b14160103a138b3f4e3620c
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2020-05-12 18:00:40 -0300

    mapeamento das rotas
    mapeamento das rotas

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,14 +3,12 @@ import 'modules/font-awesome/css/font-awesome.min.css'
 import React from 'react'
 
 // Components
-import Todo from '../todo/todo'
-import About from '../about/about'
+import Routes from './routes'
 import Menu from '../tamplete/menu'
 
 export default props => (
   <div className="container">
     <Menu />
-    <Todo />
-    <About />
+    <Routes />
   </div>
 )
