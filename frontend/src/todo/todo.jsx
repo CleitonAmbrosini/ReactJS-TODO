2b9fa2e7f67d921b16cce53e9f34c00bcb8937b6
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2020-05-13 17:39:20 -0300

    Inserção, remoção e listagem de tarefas
    Inserção, remoção e listagem de tarefas com o backend

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,17 +1,60 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 // components
 import PageHeader from '../tamplete/pageHeader'
 import TodoForm from './todoForm'
 import TodoLista from './todoList'
 
+const URL = 'http://localhost:3003/api/todos'
+
 export default class Todo extends Component{
+  constructor(props){
+    super(props)
+
+    this.state = {
+      description: '',
+      list: []
+    }
+
+    this.refresh()
+    this.handleChange = this.handleChange.bind(this)
+    this.handleAdd = this.handleAdd.bind(this)
+    this.handleRemove = this.handleRemove.bind(this)
+  }
+
+  refresh(){
+    axios.get(`${URL}?sort=-createdAt`)
+      .then(resp => {
+        this.setState({...this.state, description:'', list: resp.data})
+      })
+  }
+
+  handleChange(e){
+    this.setState({...this.state, description:e.target.value})
+  }
+
+  handleAdd() {
+    const description = this.state.description
+    axios.post(URL, {description})
+      .then(resp => {
+        this.refresh()
+      })
+  }
+
+  handleRemove(todo){
+    axios.delete(`${URL}/${todo._id}`)
+      .then(res => {
+        this.refresh()
+      })
+  }
+
   render(){
     return(
       <div>
         <PageHeader name="Tarefas" small="Cadastro"/>
-        <TodoForm />
-        <TodoLista />
+        <TodoForm handleAdd={this.handleAdd} handleChange={this.handleChange} description={this.state.description}/>
+        <TodoLista list={this.state.list} handleRemove={this.handleRemove}/>
       </div>
     )
   }
